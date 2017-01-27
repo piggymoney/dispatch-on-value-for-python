215f041b49bc3be29fbc7b7ad804cc2b344fb717
Robert Fowler <rob@piggymoney.com.au>
2017-01-27 16:41:55 +1100

    Added test calling multiple matches

--- dispatchonvalue/test/test_decorate_class_methods.py
+++ dispatchonvalue/test/test_decorate_class_methods.py
@@ -18,6 +18,16 @@ class TestDecorateClassMethod(unittest.TestCase):
                 called[0] = 2
                 return 3
 
+            @dispatch_on_value.add_method({'a': 1})
+            def m3(self, a):
+                called[0] = 3
+                return 4
+
+            @dispatch_on_value.add_method({'b': 2})
+            def m4(self, a):
+                called[0] = 4
+                return 5
+
         my_class = MyClass()
 
         called = [0]
@@ -26,3 +36,6 @@ class TestDecorateClassMethod(unittest.TestCase):
         # Should call second function above
         assert dispatch_on_value.dispatch(p) == 3
         assert called[0] == 2
+
+        assert dispatch_on_value.dispatch_all({'a': 1, 'b': 2}) == [4, 5]
+        assert dispatch_on_value.dispatch_all({'a': 3, 'b': 2}) == [5]
